4c7f01ad9fd4f8690d0e460b62d8baac26de70aa
thaitruonggg <doctormysterio060603@gmail.com>
2026-04-08 08:12:01 +0700

    refactor: Update licensing

--- PocketDrop/AppScanner.cs
+++ PocketDrop/AppScanner.cs
@@ -1,4 +1,12 @@
-﻿using Microsoft.Win32;
+﻿// PocketDrop
+// Copyright (C) 2026 Naofunyan
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// any later version.
+
+using Microsoft.Win32;
 using System;
 using System.Collections.Generic;
 using System.Drawing;
--- PocketDrop/MainWindow.xaml.cs
+++ PocketDrop/MainWindow.xaml.cs
@@ -1,4 +1,12 @@
-﻿using Microsoft.WindowsAPICodePack.Shell;
+﻿// PocketDrop
+// Copyright (C) 2026 Naofunyan
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// any later version.
+
+using Microsoft.WindowsAPICodePack.Shell;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
